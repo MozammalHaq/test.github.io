d0f2cca24744529c53940026b57497c303c4bb4c
Mozammal <mrmozammal@gmail.com>
2018-01-10 22:39:52 +0600

    Class_21

---
+++ 100ps1/Class_21/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+    $(".homepage-slides").owlCarousel({
+        items: 1,
+        nav: false,
+        dot: true,
+        loop: true,
+        autoplay: true
+    });
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
