32b14b264950e2295e507ef3891c8d03b8c38bea
Mozammal <mrmozammal@gmail.com>
2018-01-17 23:16:47 +0600

    Exam0

---
+++ psd-to-html-4/Exam0/assets/js/main.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function ($) {
+    $(".video-btn").magnificPopup({
+        type: 'video'
+    });
+    
+     $('#map')
+      .gmap3({
+        center: [23.2813894,91.1226149],
+        address: "Sadar Dakkhin, Comilla",
+        zoom: 7,
+        mapTypeId : google.maps.MapTypeId.ROADMAP,
+        scrollwheel: false,
+      })
+      .marker([
+        {position:[23.2813894,91.1226149]},
+        {address:"86000 Poitiers, France"},
+        {address:"66000 Perpignan, France", icon: "http://maps.google.com/mapfiles/marker_grey.png"}
+      ]);
+    
+})
