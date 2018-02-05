2845c8a435e83ffc5aabeb9a83e49d611f6b3a05
Mozammal Haq <mrmozammal@gmail.com>
2018-02-05 19:57:03 +0600

    Class_24

---
+++ 100ps1/class_24/assets/js/main.js
@@ -0,0 +1,29 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".homepage-slides").owlCarousel({
+            items: 1,
+            autoplay: false,
+            loop: true,
+            nav: false,
+            dots: true,
+        });
+
+
+
+
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+
+    });
+
+
+}(jQuery));
