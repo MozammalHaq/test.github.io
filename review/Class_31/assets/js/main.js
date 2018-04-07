da306d0b1b8766d55966b15e840536686037e34e
Mozammal Haq <mrmozammal@gmail.com>
2018-04-07 22:42:48 +0600

    Class_31

---
+++ review/Class_31/assets/js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+
+    var s = skrollr.init({
+        render: function(data){
+            //Debugging - log the current scroll position.
+            //colsole.log(data.curTop);
+        }
+    });
+
+    });
+
+
+    jQuery(window).load(function () {
+
+
+    });
+
+
+}(jQuery));
