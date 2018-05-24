b6dd61aa75f723eda62e4373feeacbe5ba215628
Mozammal Haq <mrmozammal@gmail.com>
2018-05-24 22:54:40 +0600

    Class-46

---
+++ 100ps1/Class-46/assets/js/main.js
@@ -0,0 +1,12 @@
+(function ($){
+    "use strict";
+    jQuery(document).ready(function($){
+        $(".featured-trailors-caurosel").owlCarousel({
+            items: 1,
+            nav: false,
+            dot: true,
+            loop: true,
+            autoplay: true,
+        });
+    })
+}(jQuery));
