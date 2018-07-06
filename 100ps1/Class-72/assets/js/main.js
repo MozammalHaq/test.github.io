d52c35a4af15fe49a5de3f3e153e7329bbf8f6d6
Mozammal Haq <mrmozammal@gmail.com>
2018-07-06 21:11:59 +0600

    Class-72

---
+++ 100ps1/Class-72/assets/js/main.js
@@ -0,0 +1,11 @@
+(function ($){
+    "use strict";
+    jQuery(document).ready(function($){
+
+        $('.count-number span').counterUp({
+            delay: 10,
+            time: 1000,
+        });
+
+    })
+}(jQuery));
