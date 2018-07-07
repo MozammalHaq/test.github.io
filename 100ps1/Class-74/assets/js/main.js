340172035d448a1c107152720ccbe2ec8afb7765
Mozammal Haq <mrmozammal@gmail.com>
2018-07-07 16:54:21 +0600

    Class-74

---
+++ 100ps1/Class-74/assets/js/main.js
@@ -0,0 +1,10 @@
+(function ($) {
+    "use strict";
+    jQuery(document).ready(function ($) {
+
+        $('#clock').countdown('2020/01/03', function (event) {
+            $(this).html(event.strftime('<span class="countdown-wrap"><span class="single-countdown-item">%w <br/><span>Weeks</span></span><span class="single-countdown-item">%d <br/><span>Days</span></span><span class="single-countdown-item">%H <br/><span>Hours</span></span><span class="single-countdown-item">%M <br/><span>Minutes</span></span><span class="single-countdown-item">%S <br/><span>Seconds</span></span></span>'));
+        });
+
+    })
+}(jQuery));
