5e11d2e59c8c1181abb481b416857190bc58cd76
Mozammal Haq <mrmozammal@gmail.com>
2018-07-10 12:11:31 +0600

    Class-82

---
+++ 100ps1/Class-82/assets/js/main.js
@@ -0,0 +1,14 @@
+(function ($) {
+    "use strict";
+    jQuery(document).ready(function ($) {
+
+        $(".feature-project").owlCarousel({
+            items: 1,
+            loop: true,
+            nav: false,
+            dot: true,
+            autoplay: true,
+        });
+
+    })
+}(jQuery));
