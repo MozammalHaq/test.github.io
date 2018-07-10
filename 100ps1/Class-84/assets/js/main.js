ee77d9106203e309084796507783833c951e846a
Mozammal Haq <mrmozammal@gmail.com>
2018-07-10 21:59:43 +0600

    Class-84

---
+++ 100ps1/Class-84/assets/js/main.js
@@ -0,0 +1,12 @@
+(function ($){
+    "use strict";
+    jQuery(document).ready(function($){
+
+        $('.hover').hover(function(){
+            $(this).addClass('flip');
+        },function(){
+            $(this).removeClass('flip');
+        });
+
+    })
+}(jQuery));
