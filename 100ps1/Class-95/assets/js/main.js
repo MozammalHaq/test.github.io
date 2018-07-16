d1759dd98122b99249ff7540558964a42eac92dc
Mozammal Haq <mrmozammal@gmail.com>
2018-07-16 16:13:06 +0600

    Class-95

---
+++ 100ps1/Class-95/assets/js/main.js
@@ -0,0 +1,14 @@
+(function ($){
+    "use strict";
+    jQuery(document).ready(function($){
+
+        $(".slider-area").owlCarousel({
+            items: 1,
+            nav: false,
+            dots: true,
+            loop: true,
+            autoplay: true
+        });
+
+    });
+}(jQuery));
